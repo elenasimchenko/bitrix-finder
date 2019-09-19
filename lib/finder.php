201450d913cb871b980c253bbffa2706928c4001
Elena Simchenko <33508894+elenasimchenko@users.noreply.github.com>
2019-09-19 15:01:55 +0300

    reorganaize

--- lib/finder.php
+++ lib/finder.php
@@ -1,50 +1,25 @@
 <?php
+namespace Finder
+
 use Bitrix\Main\Loader;
 
-class Finder {
-
+class Site {
   /**
+   * Site Url
+   * @param string $siteId
+   * @return string
+   */
+  public static function getDomainUrl($siteId, $protocol = 'https'):string {
+      return $protocol.'://'.\CSite::GetByID($siteId)->Fetch()['SERVER_NAME'];
+  }
+  
+    /**
    * Site domain name
    * @param string $siteId
    * @return string
    */
-  public static function findDomain($siteId, $protocol = 'https'):string {
-      return $protocol.'://'.\CSite::GetByID($siteId)->Fetch()['SERVER_NAME'];
-  }
-  
-  /**
-   * $code could be IBLOCK_CODE or IBLOCK_TYPE_ID:IBLOCK_CODE
-   * @param $code
-   * @return int
-   */
-  public static function findIblock(string $code):int {
-
-      try {
-          Loader::includeModule('iblock');
-      } catch (\Exception $e) {
-          return false;
-      }
-
-      if (false !== strpos($code, ':')) {
-          list($typeCode, $iblockCode) = explode(':', $code);
-          $filter['IBLOCK_TYPE_ID'] = $typeCode;
-          $filter['CODE'] = $iblockCode;
-      } else {
-          $filter = [
-              'CODE' => $code
-          ];
-      }
-
-      $data = IblockTable::getRow([
-          'filter' => $filter,
-          'select' => [
-              'ID'
-          ],
-          'cache' => [
-              'ttl' => 3600
-          ]
-      ]);
-
-      return $data ? (int)$data['ID'] : 0;
+  public static function getDomain($siteId, $protocol = 'https'):string {
+      return \CSite::GetByID($siteId)->Fetch()['SERVER_NAME'];
   }
 }
+
